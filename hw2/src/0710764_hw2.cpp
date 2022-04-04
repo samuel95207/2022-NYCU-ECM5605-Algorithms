c511f486c5b61b1ed476bf3f1b8f4a75201e5ea5
Samuel Hunag <lspss95207@gmail.com>
2022-04-04 17:27:10 +0800

    Reduce memory

--- hw2/src/0710764_hw2.cpp
+++ hw2/src/0710764_hw2.cpp
@@ -13,6 +13,15 @@
 
 using namespace std;
 
+
+struct ValuePosPair {
+    double value;
+    long pos;
+};
+
+bool valuePosPaircompare(ValuePosPair& a, ValuePosPair& b) { return a.value < b.value; }
+
+
 template <typename T>
 void printVector(const T& t) {
     std::copy(t.cbegin(), t.cend(), std::ostream_iterator<typename T::value_type>(std::cout, " "));
@@ -20,25 +29,81 @@ void printVector(const T& t) {
 }
 
 
-void InsertionSort(std::vector<double>& arr, std::vector<long>& mapArr, std::vector<long>& originalPosArr, int begin,
-                   int end) {
+void InsertionSort(std::vector<ValuePosPair>& arr, int begin, int end) {
     for (int i = begin + 1; i < end; i++) {
-        int j = i;
-        while (j > begin && arr[originalPosArr[j - 1]] > arr[originalPosArr[j]]) {
-            swap(mapArr[originalPosArr[j - 1]], mapArr[originalPosArr[j]]);
-            swap(originalPosArr[j - 1], originalPosArr[j]);
-            j--;
+        auto key = arr[i];
+        int j = i - 1;
+
+        while (j >= begin && arr[j].value > key.value) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
         }
+        arr[j + 1] = key;
     }
+
+    // for (int i = begin + 1; i < end; i++) {
+    //     int j = i;
+    //     while (j > begin && arr[originalPosArr[j - 1]] > arr[originalPosArr[j]]) {
+    //         swap(mapArr[originalPosArr[j - 1]], mapArr[originalPosArr[j]]);
+    //         swap(originalPosArr[j - 1], originalPosArr[j]);
+    //         j--;
+    //     }
+    // }
+
+    // for (int i = begin + 1; i < end; i++) {
+    //     int j = i;
+    //     int jPos = originalPosArr[j];
+    //     int jMinusPos = originalPosArr[j - 1];
+    //     while (j > begin && arr[jMinusPos] > arr[jPos]) {
+    //         swap(mapArr[jMinusPos], mapArr[jPos]);
+    //         swap(originalPosArr[j - 1], originalPosArr[j]);
+    //         j--;
+
+    //         jPos = originalPosArr[j];
+    //         jMinusPos = originalPosArr[j - 1];
+    //     }
+    // }
+
+
+    // for (int i = begin + 1; i < end; i++) {
+    //     int keyOriginalPos = originalPosArr[i];
+    //     double key = arr[keyOriginalPos];
+
+    //     int j = i - 1;
+    //     while (j >= 0 && arr[originalPosArr[j]] > key) {
+    //         mapArr[originalPosArr[j]] = j + 1;
+    //         originalPosArr[j + 1] = originalPosArr[j];
+    //         j--;
+    //     }
+    //     mapArr[keyOriginalPos] = j + 1;
+    //     originalPosArr[j + 1] = keyOriginalPos;
+    // }
+
+    // for (int i = begin + 1; i < end; i++) {
+    //     int keyOriginalPos = originalPosArr[i];
+    //     double key = arr[keyOriginalPos];
+
+    //     int j = i - 1;
+    //     int jPos = originalPosArr[j];
+    //     while (j >= 0 && arr[jPos] > key) {
+    //         mapArr[jPos] = j + 1;
+    //         originalPosArr[j + 1] = jPos;
+    //         j--;
+
+    //         jPos = originalPosArr[j];
+    //     }
+    //     mapArr[keyOriginalPos] = j + 1;
+    //     originalPosArr[j + 1] = keyOriginalPos;
+    // }
 
     return;
 }
 
 
-int medianIdx(std::vector<double>& arr, int aIdx, int bIdx, int cIdx) {
-    int a = arr[aIdx];
-    int b = arr[bIdx];
-    int c = arr[cIdx];
+int medianIdx(std::vector<ValuePosPair>& arr, int aIdx, int bIdx, int cIdx) {
+    int a = arr[aIdx].value;
+    int b = arr[bIdx].value;
+    int c = arr[cIdx].value;
 
     if (a <= b && b <= c) {
         return bIdx;
@@ -62,8 +127,7 @@ int medianIdx(std::vector<double>& arr, int aIdx, int bIdx, int cIdx) {
 }
 
 
-int Partition(std::vector<double>& arr, std::vector<long>& mapArr, std::vector<long>& originalPosArr, int begin,
-              int end) {
+int Partition(std::vector<ValuePosPair>& arr, int begin, int end) {
     // Three random Idx
     int randIdx1 = rand() % (end - begin) + begin;
     int randIdx2 = rand() % (end - begin) + begin;
@@ -76,34 +140,30 @@ int Partition(std::vector<double>& arr, std::vector<long>& mapArr, std::vector<l
     // One random Idx
     // int randIdx = rand() % (end - begin) + begin;
 
-    swap(mapArr[originalPosArr[randIdx]], mapArr[originalPosArr[end - 1]]);
-    swap(originalPosArr[randIdx], originalPosArr[end - 1]);
+    swap(arr[randIdx], arr[end - 1]);
 
-    double pivot = arr[originalPosArr[end - 1]];
+    double pivot = arr[end - 1].value;
     int i = begin - 1;
     for (int j = begin; j < end - 1; j++) {
-        if (arr[originalPosArr[j]] < pivot) {
+        if (arr[j].value < pivot) {
             i++;
-            swap(mapArr[originalPosArr[i]], mapArr[originalPosArr[j]]);
-            swap(originalPosArr[i], originalPosArr[j]);
+            swap(arr[i], arr[j]);
         }
     }
     i++;
-    swap(mapArr[originalPosArr[i]], mapArr[originalPosArr[end - 1]]);
-    swap(originalPosArr[i], originalPosArr[end - 1]);
+    swap(arr[i], arr[end - 1]);
     return i;
 }
 
-void QuickSort(std::vector<double>& arr, std::vector<long>& mapArr, std::vector<long>& originalPosArr, int begin,
-               int end) {
+void QuickSort(std::vector<ValuePosPair>& arr, int begin, int end) {
     int size = end - begin;
     if (size > 1) {
         if (size < INSERTION_SORT_THRESHOLD) {
-            InsertionSort(arr, mapArr, originalPosArr, begin, end);
+            InsertionSort(arr, begin, end);
         } else {
-            int pivot = Partition(arr, mapArr, originalPosArr, begin, end);
-            QuickSort(arr, mapArr, originalPosArr, begin, pivot);
-            QuickSort(arr, mapArr, originalPosArr, pivot + 1, end);
+            int pivot = Partition(arr, begin, end);
+            QuickSort(arr, begin, pivot);
+            QuickSort(arr, pivot + 1, end);
         }
 
         // int pivot = Partition(arr, mapArr, originalPosArr, begin, end);
@@ -113,8 +173,7 @@ void QuickSort(std::vector<double>& arr, std::vector<long>& mapArr, std::vector<
 }
 
 
-void heapify(std::vector<double>& arr, std::vector<long>& mapArr, std::vector<long>& originalPosArr, int begin, int end,
-             int i) {
+void heapify(std::vector<ValuePosPair>& arr, int begin, int end, int i) {
     int n = end - begin;
 
     int largest = i;
@@ -122,177 +181,136 @@ void heapify(std::vector<double>& arr, std::vector<long>& mapArr, std::vector<lo
     int r = 2 * i + 2;
 
 
-    if (l < n && arr[originalPosArr[l + begin]] > arr[originalPosArr[largest + begin]]) {
+    if (l < n && arr[l + begin].value > arr[largest + begin].value) {
         largest = l;
     }
 
 
-    if (r < n && arr[originalPosArr[r + begin]] > arr[originalPosArr[largest + begin]]) {
+    if (r < n && arr[r + begin].value > arr[largest + begin].value) {
         largest = r;
     }
 
 
     if (largest != i) {
-        swap(mapArr[originalPosArr[i + begin]], mapArr[originalPosArr[largest + begin]]);
-        swap(originalPosArr[i + begin], originalPosArr[largest + begin]);
-        heapify(arr, mapArr, originalPosArr, begin, end, largest);
+        swap(arr[i + begin], arr[largest + begin]);
+        heapify(arr, begin, end, largest);
     }
 }
 
-void HeapSort(std::vector<double>& arr, std::vector<long>& mapArr, std::vector<long>& originalPosArr, int begin,
-              int end) {
+void HeapSort(std::vector<ValuePosPair>& arr, int begin, int end) {
     int size = end - begin;
     for (int i = size / 2 - 1; i >= 0; i--) {
-        heapify(arr, mapArr, originalPosArr, begin, end, i);
+        heapify(arr, begin, end, i);
     }
 
 
     for (int i = size - 1; i > 0; i--) {
-        swap(mapArr[originalPosArr[i + begin]], mapArr[originalPosArr[0 + begin]]);
-        swap(originalPosArr[i + begin], originalPosArr[0 + begin]);
-        heapify(arr, mapArr, originalPosArr, begin, begin + i, 0);
+        swap(arr[i + begin], arr[begin]);
+        heapify(arr, begin, begin + i, 0);
     }
 }
 
 
-void IntrosortUtil(std::vector<double>& arr, std::vector<long>& mapArr, std::vector<long>& originalPosArr, int begin,
-                   int end, int depthLimit) {
+void IntrosortUtil(std::vector<ValuePosPair>& arr, int begin, int end, int depthLimit) {
     // Count the number of elements
     int size = end - begin;
 
     // If partition size is low then do insertion sort
     if (size < INSERTION_SORT_THRESHOLD) {
-        InsertionSort(arr, mapArr, originalPosArr, begin, end);
+        InsertionSort(arr, begin, end);
         return;
     }
 
     // If the depth is zero use heapsort
     if (depthLimit == 0) {
-        HeapSort(arr, mapArr, originalPosArr, begin, end);
+        HeapSort(arr, begin, end);
         return;
     }
 
 
     // Quick Sort
-    int pivot = Partition(arr, mapArr, originalPosArr, begin, end);
-    IntrosortUtil(arr, mapArr, originalPosArr, begin, pivot, depthLimit - 1);
-    IntrosortUtil(arr, mapArr, originalPosArr, pivot + 1, end, depthLimit - 1);
-
-    // IntrosortUtil(arr, begin, partitionPoint - 1, depthLimit - 1);
-    // IntrosortUtil(arr, partitionPoint + 1, end, depthLimit - 1);
+    int pivot = Partition(arr, begin, end);
+    IntrosortUtil(arr, begin, pivot, depthLimit - 1);
+    IntrosortUtil(arr, pivot + 1, end, depthLimit - 1);
 
     return;
 }
 
 
 
-void IntroSort(std::vector<double>& arr, std::vector<long>& mapArr, std::vector<long>& originalPosArr, int begin,
-               int end) {
+void IntroSort(std::vector<ValuePosPair>& arr, int begin, int end) {
     int depthLimit = 2 * log(end - begin);
 
     // Perform a recursive Introsort
-    IntrosortUtil(arr, mapArr, originalPosArr, begin, end, depthLimit);
+    IntrosortUtil(arr, begin, end, depthLimit);
 
     return;
 }
 
 
 
-void merge(std::vector<double>& arr, std::vector<long>& mapArr, std::vector<long>& originalPosArr, int l, int m,
-           int r) {
+void merge(std::vector<ValuePosPair>& arr, int l, int m, int r) {
     int len1 = m - l + 1, len2 = r - m;
 
-    int* leftOriginalPosArr = new int[len1];
-    int* rightOriginalPosArr = new int[len2];
-
+    auto leftArr = new ValuePosPair[len1];
+    auto rightArr = new ValuePosPair[len2];
 
     for (int i = 0; i < len1; i++) {
-        leftOriginalPosArr[i] = originalPosArr[l + i];
+        leftArr[i] = arr[l + i];
     }
     for (int i = 0; i < len2; i++) {
-        rightOriginalPosArr[i] = originalPosArr[m + 1 + i];
+        rightArr[i] = arr[m + 1 + i];
     }
-
-    // cout << "left ";
-    // for (int i = 0; i < len1; i++) {
-    //     cout << arr[leftOriginalPosArr[i]] << " ";
-    // }
-    // cout << endl << "right ";
-    // for (int i = 0; i < len2; i++) {
-    //     cout << arr[rightOriginalPosArr[i]] << " ";
-    // }
-    // cout << endl;
-
-
 
 
     int i = 0;
     int j = 0;
     int k = l;
 
-    // cout << "len1=" << len1 << " len2=" << len2 << endl;
     while (i < len1 && j < len2) {
-        // cout << "i=" << i << " j=" << j << " arr[i]=" << arr[leftOriginalPosArr[i]]
-        //      << " arr[j]=" << arr[rightOriginalPosArr[j]];
-        if (arr[leftOriginalPosArr[i]] <= arr[rightOriginalPosArr[j]]) {
-            int originPos = leftOriginalPosArr[i];
-            // cout << " choose " << arr[originPos] << endl;
-            mapArr[originPos] = k;
-            originalPosArr[k] = originPos;
+        if (leftArr[i].value <= rightArr[j].value) {
+            arr[k] = leftArr[i];
             i++;
         } else {
-            int originPos = rightOriginalPosArr[j];
-            // cout << " choose " << arr[originPos] << endl;
-            mapArr[originPos] = k;
-            originalPosArr[k] = originPos;
+            arr[k] = rightArr[j];
             j++;
         }
         k++;
     }
 
     while (i < len1) {
-        int originPos = leftOriginalPosArr[i];
-        mapArr[originPos] = k;
-        originalPosArr[k] = originPos;
+        arr[k] = leftArr[i];
         k++;
         i++;
     }
 
     while (j < len2) {
-        int originPos = rightOriginalPosArr[j];
-        mapArr[originPos] = k;
-        originalPosArr[k] = originPos;
+        arr[k] = rightArr[j];
         k++;
         j++;
     }
 
-    // for (int i = l; i <= r; i++) {
-    //     cout << arr[mapArr[i]] << " ";
-    // }
-    // cout << endl << endl;
-
-    delete leftOriginalPosArr;
-    delete rightOriginalPosArr;
+    delete leftArr;
+    delete rightArr;
 }
 
 
-void MergeSort(std::vector<double>& arr, std::vector<long>& mapArr, std::vector<long>& originalPosArr, int begin,
-               int end) {
+void MergeSort(std::vector<ValuePosPair>& arr, int begin, int end) {
     if (begin >= end - 1) return;
 
     int mid = begin + (end - begin - 1) / 2;
-    // cout << "split " << begin << " - " << mid << " " << mid + 1 << " - " << end - 1 << endl;
-    MergeSort(arr, mapArr, originalPosArr, begin, mid + 1);
-    MergeSort(arr, mapArr, originalPosArr, mid + 1, end);
-    merge(arr, mapArr, originalPosArr, begin, mid, end - 1);
+    MergeSort(arr, begin, mid + 1);
+    MergeSort(arr, mid + 1, end);
+    merge(arr, begin, mid, end - 1);
 }
 
 
-void TimSort(std::vector<double>& arr, std::vector<long>& mapArr, std::vector<long>& originalPosArr) {
+
+void TimSort(std::vector<ValuePosPair>& arr) {
     int n = arr.size();
 
     for (int i = 0; i < n; i += TIM_SORT_RUN) {
-        InsertionSort(arr, mapArr, originalPosArr, i, min((i + TIM_SORT_RUN), n));
+        InsertionSort(arr, i, min((i + TIM_SORT_RUN), n));
     }
 
     for (int size = TIM_SORT_RUN; size < n; size = 2 * size) {
@@ -300,12 +318,11 @@ void TimSort(std::vector<double>& arr, std::vector<long>& mapArr, std::vector<lo
             int mid = left + size - 1;
             int right = min((left + 2 * size - 1), (n - 1));
             if (mid < right) {
-                merge(arr, mapArr, originalPosArr, left, mid, right);
+                merge(arr, left, mid, right);
             }
         }
     }
 }
-
 
 
 
@@ -314,41 +331,40 @@ std::vector<long> my_sort(std::vector<long> input) {
     srand(0);
 
     int size = input.size();
-    std::vector<double> doubleInput;
-    std::vector<long> originalPos;
+    std::vector<ValuePosPair> inputPairs;
     std::vector<long> result;
 
-    doubleInput.resize(size);
-    originalPos.resize(size);
+    inputPairs.resize(size);
     result.resize(size);
 
     for (int i = 0; i < size; i++) {
-        doubleInput[i] = f(input[i]);
-        originalPos[i] = i;
-        result[i] = i;
+        auto inputPairPtr = &inputPairs[i];
+        inputPairPtr->value = f(input[i]);
+        inputPairPtr->pos = i;
     }
 
-    // printVector(doubleInput);
-
-    // sort(doubleInput.begin(), doubleInput.end());
-
-
-    // InsertionSort(doubleInput, result, originalPos, 0, size);
-    // QuickSort(doubleInput, result, originalPos, 0, size);
-    // IntroSort(doubleInput, result, originalPos, 0, size);
-    // HeapSort(doubleInput, result, originalPos, 0, size);
-    // MergeSort(doubleInput, result, originalPos, 0, size);
-    TimSort(doubleInput, result, originalPos);
-
-
-    // std::vector<double> sortedArr;
     // for (int i = 0; i < size; i++) {
-    //     sortedArr.push_back(doubleInput[result[i]]);
+    //     cout << inputPairs[i].value << " ";
     // }
-    // printVector(sortedArr);
-    // printVector(result);
+    // cout << endl;
+
+    // InsertionSort(inputPairs, 0, size);
+    // QuickSort(inputPairs, 0, size);
+    // IntroSort(inputPairs, 0, size);
+    // HeapSort(inputPairs, 0, size);
+    // MergeSort(inputPairs, 0, size);
+    TimSort(inputPairs);
+    // sort(inputPairs.begin(), inputPairs.end(), valuePosPaircompare);
 
 
+    // for (int i = 0; i < size; i++) {
+    //     result[inputPairs[i].pos] = i;
+    // }
+
+    // for (int i = 0; i < size; i++) {
+    //     cout << inputPairs[i].value << " ";
+    // }
+    // cout << endl;
 
 
     return result;
